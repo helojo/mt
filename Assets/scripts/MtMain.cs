a078506f958e238e47aecb678d76283587bbebfc
helojo <admin@helojo.com>
2015-03-12 22:56:38 +0800

    add by me

---
+++ Assets/scripts/MtMain.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MtMain : MonoBehaviour {
+
+	// Use this for initialization
+    bool isStartGame;
+
+	void Start () {
+        Debug.Log("GameStarted");
+	}
+
+    void OnGUI()
+    {
+        if (!isStartGame)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 25, Screen.height / 2 - 15, 80, 30), "开始战斗"))
+            {
+                isStartGame = true;
+                Debug.Log("开始战斗了");
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
